ca0ff07a885a7ef46d2811fe3121015a2fe36290
nieshiqing <>
2019-11-12 12:01:22 +0800

    开启gzip压缩和路由懒加载

--- src/router.js
+++ src/router.js
@@ -5,49 +5,50 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path:'/signIn',
-      name:'signIn',
-      component:()=>import('./views/SignIn.vue')
-    },
-    {
-      path:'/query',
-      name:'query',
-      component:()=>import('./views/Query.vue')
-    },
-    {
-      path:'/register',
-      name:'register',
-      component:()=>import('./views/Register.vue')
-    },
-    {
-      path:'/a',
-      name:'a',
-      component:()=>import('./views/Register.vue')
-    },
-    {
-      path:'/b',
-      name:'b',
-      component:()=>import('./views/Register.vue')
-    },
-    {
-      path:'/c',
-      name:'c',
-      component:()=>import('./views/Register.vue')
-    },
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: resolve => require([Home], resolve)
+            // component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        },
+        {
+            path: '/signIn',
+            name: 'signIn',
+            component: () => import('./views/SignIn.vue')
+        },
+        {
+            path: '/query',
+            name: 'query',
+            component: () => import('./views/Query.vue')
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: () => import('./views/Register.vue')
+        },
+        {
+            path: '/a',
+            name: 'a',
+            component: () => import('./views/Register.vue')
+        },
+        {
+            path: '/b',
+            name: 'b',
+            component: () => import('./views/Register.vue')
+        },
+        {
+            path: '/c',
+            name: 'c',
+            component: () => import('./views/Register.vue')
+        },
+    ]
 })
--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,46 @@
 var path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const productionGzipExtensions = ['js', 'css'];
 module.exports = {
-  publicPath: isProduction ? '/' : '/',
-  outputDir: "dist",
-  filenameHashing: true,
-  devServer: {                // 本地开发配置
-    host: '39.105.159.181',
-    port: 8095,
-    https: false,
-    hotOnly: false,
-    proxy: 'http://39.105.159.181:6789', // 设置代理
-    // proxy: 'http://127.0.0.1:6789',
-    before: app => { }
-  },
-  configureWebpack: config => {
-    if (isProduction) {
-      // 为生产环境修改配置...
-      config.plugins.push(
-        //生产环境自动删除console
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            compress: {
-              drop_debugger: true,
-              drop_console: true,
-            },
-          },
-          sourceMap: false,
-          parallel: 4,
-        })
-      );
-    }
-  },
+    publicPath: isProduction ? '/' : '/',
+    outputDir: "dist",
+    filenameHashing: true,
+    devServer: {                // 本地开发配置
+        host: '39.98.156.123',
+        // host: 'localhost',
+        port: 8095,
+        https: false,
+        hotOnly: false,
+        proxy: 'http://39.98.156.123:6789', // 设置代理
+        // proxy: 'http://127.0.0.1:6789',
+        before: app => {
+        }
+    },
+    configureWebpack: config => {
+        if (isProduction) {
+            // 为生产环境修改配置...
+            config.plugins.push(
+                //生产环境开启gzip压缩
+                new CompressionWebpackPlugin({
+                    algorithm: 'gzip',
+                    test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+                    threshold: 10240,
+                    minRatio: 0.8
+                }),
+                //生产环境自动删除console
+                new UglifyJsPlugin({
+                    uglifyOptions: {
+                        compress: {
+                            drop_debugger: true,
+                            drop_console: true,
+                        },
+                    },
+                    sourceMap: false,
+                    parallel: 4,
+                })
+            );
+        }
+    },
 }
